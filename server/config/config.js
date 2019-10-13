c496a626ea821c979f97f8dade48cd4a375cc72f
Joao García <jogu2980@gmail.com>
2019-10-13 17:45:54 -0500

    RestServer usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,27 @@
+// import { url } from "inspector";
+
+// ==================================
+//                 PORT
+// ==================================
+
 process.env.PORT = process.env.PORT || 3000;
+
+// ==================================
+//              Entorno
+// ==================================
+
+process.env.NODE_EVN = process.env.NODE_EVN || 'dev';
+
+// ==================================
+//          Base de datos
+// ==================================
+
+let urlDB;
+
+if (process.env.NODE_EVN === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://joguMaster16:Sj2rQSliM7P4pFEC@cluster0-2yzbu.mongodb.net/cafe'
+}
+
+process.env.URLDB = urlDB;
